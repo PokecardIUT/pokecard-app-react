1fe4ce11880c39021085d18e87dadb01df6d82cd
lpiem <loic.ronzier@etu.univ-lyon1.fr>
2019-03-11 17:41:03 +0100

    [FEAT] custom navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import * as React from "react";
+import { Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default () => (
+// const HomePage = () => (
+//   <View style={{ marginTop: 50 }}>
+//     <Text>Hello Expo! Cocuou</Text>
+//   </View>
+// );
+
+const HomePage = () => (
   <View style={{ marginTop: 50 }}>
     <Text>Hello Expo! Cocuou</Text>
   </View>
-);
+)
+
+const Navigator = createStackNavigator({
+  HomePage: {
+    screen: HomePage,
+    navigationOptions: {
+      title: "List Deck",
+      headerStyle: {
+        backgroundColor: "#f4511e"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      }
+    }
+  }
+});
+
+export default createAppContainer(Navigator);
