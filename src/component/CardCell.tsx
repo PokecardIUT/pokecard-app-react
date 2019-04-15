0d97bbb1eb37af0d34aa54394e1e98e6d53401e2
Loic <Loic-0607@hotmail.fr>
2019-04-15 23:40:09 +0200

    [TASK] Change add colmn to flatList and add StyleSheet

--- src/component/CardCell.tsx
+++ src/component/CardCell.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
 import { Cards } from "../model/Card";
-import { Card, Image } from "react-native-elements";
-import { Text } from "react-native";
+import { Image } from "react-native-elements";
+import { Dimensions, StyleSheet } from "react-native";
 
 interface MyProp {
   item: Cards;
 }
 
 export const CardCell = (props: MyProp) => (
-  <Card>
-    <Image
-      source={{ uri: props.item.imageUrl }}
-      style={{ resizeMode: "contain", width: 150, height: 200 }}
-    />
-  </Card>
+  <Image source={{ uri: props.item.imageUrl }} style={style.card} />
 );
+
+const style = StyleSheet.create({
+  card: {
+    resizeMode: "contain",
+    width: Dimensions.get("window").width / 3,
+    height: Dimensions.get("window").width / 2.5,
+    margin: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
--- src/component/ListCard.tsx
+++ src/component/ListCard.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Cards } from "../model/Card";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { CardCell } from "./CardCell";
 
 interface MyProps {
@@ -10,10 +10,19 @@ interface MyProps {
 export const ListCard = (props: MyProps) => {
   console.log(props.data);
   return (
-    <FlatList
-      data={props.data}
-      renderItem={({ item }) => <CardCell item={item} />}
-      keyExtractor={item => item.id}
-    />
+    <View style={style.marginTop}>
+      <FlatList
+        data={props.data}
+        renderItem={({ item }) => <CardCell item={item} />}
+        keyExtractor={item => item.id}
+        numColumns={3}
+      />
+    </View>
   );
 };
+
+const style = StyleSheet.create({
+  marginTop: {
+    marginTop: 8
+  }
+});
