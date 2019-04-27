9af407acd2c769ff9909a0c148fcfce4524d79d5
Loic <Loic-0607@hotmail.fr>
2019-04-27 22:59:42 +0200

    [FEAT] Add modal and clean

--- src/component/ListSets.tsx
+++ src/component/ListSets.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { Sets } from "../model/Sets";
-import { FlatList, View, TouchableOpacity } from "react-native";
+import { FlatList, View } from "react-native";
 import { SetCell } from "./SetCell";
 import { NavigationProps } from "../App";
-import { Icon } from "react-native-elements";
 
 interface MyProps {
   data: Sets[];
